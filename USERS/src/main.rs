ee19a9961d2b5134e70a3c80c83bd4358fae98b0
Bryan G <b.gonnella94@gmail.com>
2024-10-07 18:34:27 -0700

    Function USERS GET method with Cognito authentication and IAM-based RDS access

---
+++ USERS/src/main.rs
@@ -0,0 +1,190 @@
+use std::env;
+use std::time::{Duration, SystemTime};
+use serde_json::json;
+use sqlx::FromRow;
+use sqlx::postgres::PgConnectOptions;
+use http::HeaderMap;
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use aws_credential_types::provider::ProvideCredentials;
+use aws_config::BehaviorVersion;
+use aws_lambda_events::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_sigv4::{
+    http_request::{sign, SignableBody, SignableRequest, SigningSettings},
+    sign::v4,
+};
+
+#[derive(Clone, FromRow)]
+struct UserInfo {
+    first_name: String,
+    last_name: String,
+    conditions: Vec<bool>
+}
+
+struct UserUpload {
+    first_name: String,
+    last_name: String,
+    email: String,
+    documents: Vec<String>, // Vector of filenames
+    conditions: Vec<bool>
+}
+
+const RDS_CERTS: &[u8] = include_bytes!("global-bundle.pem");
+
+async fn generate_rds_iam_token(
+    db_hostname: &str,
+    port: u16,
+    db_username: &str,
+) -> Result<String, Error> {
+    let config = aws_config::load_defaults(BehaviorVersion::v2024_03_28()).await;
+    let credentials = config
+        .credentials_provider()
+        .expect("Credential provider provisioning")
+        .provide_credentials()
+        .await
+        .expect("Load credentials");
+    let identity = credentials.into();
+    let region = config.region().unwrap().to_string();
+
+    let mut signing_settings = SigningSettings::default();
+    signing_settings.expires_in = Some(Duration::from_secs(900));
+    signing_settings.signature_location = aws_sigv4::http_request::SignatureLocation::QueryParams;
+
+    let signing_params = v4::SigningParams::builder()
+        .identity(&identity)
+        .region(&region)
+        .name("rds-db")
+        .time(SystemTime::now())
+        .settings(signing_settings)
+        .build()?;
+
+    let url = format!(
+        "https://{db_hostname}:{port}/?Action=connect&DBUser={db_user}",
+        db_hostname = db_hostname,
+        port = port,
+        db_user = db_username
+    );
+
+    let signable_request =
+        SignableRequest::new("GET", &url, std::iter::empty(), SignableBody::Bytes(&[]))
+            .expect("signable request");
+
+    let (signing_instructions, _signature) =
+        sign(signable_request, &signing_params.into())?.into_parts();
+
+    let mut url = url::Url::parse(&url).unwrap();
+    for (name, value) in signing_instructions.params() {
+        url.query_pairs_mut().append_pair(name, &value);
+    }
+
+    let response = url.to_string().split_off("https://".len());
+
+    Ok(response)
+}
+
+async fn get_user(db_connection: &sqlx::PgPool, customer_id: &String, user_id: &String) -> Option<UserInfo> {
+    let user_exists = r#"
+        SELECT u.first_name, u.last_name, u.conditions
+        FROM "Users" AS u
+        JOIN "CustomerUsers" AS cu ON u.id = cu.user_id
+        WHERE cu.id = $1
+            AND cu.user_id = $2
+    "#;
+
+    let query = sqlx::query_as::<_, UserInfo>(user_exists)
+        .bind(customer_id)
+        .bind(user_id);
+    
+    let users: Vec<UserInfo> = query
+        .fetch_all(db_connection)
+        .await
+        .expect("Database user query");
+
+    println!("Found {} users", users.len());
+    
+    match users.len() {
+        0 => None,
+        1 => Some(users[0].clone()),
+        _ => panic!("Received too many matching rows. Suspected injection!")
+    }
+}
+
+fn response(status_code: i64, message: &str) -> ApiGatewayProxyResponse{
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+    return ApiGatewayProxyResponse {
+        multi_value_headers: headers.clone(),
+        is_base64_encoded: false,
+        body: Some(json!({"message": message}).to_string().into()),
+        status_code: status_code,
+        headers
+    };
+}
+
+async fn handler(event: LambdaEvent<ApiGatewayProxyRequest>) 
+                    -> Result<ApiGatewayProxyResponse, Error> {
+    // Read env variables
+    let db_host = env::var("DB_HOSTNAME").expect("DB_HOSTNAME must be set");
+    let db_port = env::var("DB_PORT")
+        .expect("DB_PORT must be set")
+        .parse::<u16>()
+        .expect("PORT must be a valid number");
+    let db_name = env::var("DB_NAME").expect("DB_NAME must be set");
+    let db_username = env::var("DB_USERNAME").expect("DB_USERNAME must be set");
+
+    // Generate IAM token for RDS connection
+    let token = generate_rds_iam_token(&db_host, db_port, &db_username)
+        .await
+        .expect("TOKEN generation");
+
+    // Connect to RDS Postgres DB
+    let opts = PgConnectOptions::new()
+        .host(&db_host)
+        .port(db_port)
+        .username(&db_username)
+        .password(&token)
+        .database(&db_name)
+        .ssl_root_cert_from_pem(RDS_CERTS.to_vec())
+        .ssl_mode(sqlx::postgres::PgSslMode::Require);
+
+    let pool = sqlx::postgres::PgPoolOptions::new()
+        .connect_with(opts)
+        .await
+        .expect("PGPOOL CONNECT");
+
+    // Read API Gateway data
+    let user_query = &event.payload.query_string_parameters;
+    let req_method = &event.payload.request_context.http_method;
+    let customer_sub = &event.payload.request_context.authorizer.fields
+        .get("claims")
+        .and_then(|claims| claims.get("sub"))
+        .map(|sub| sub.to_string())
+        .unwrap_or("Could not identify requestor.".to_string())
+        .replace("\"",""); // Filter out the quote characters pulled from the json data
+    
+   
+    // TEST LOCATION
+    let mut message: String = String::new();
+    let id = match user_query.first("id") {
+        Some(istr) => istr,
+        None => ""
+    }.to_string();
+    if id.is_empty() {
+        return Ok(response(400, "Could not identify query ID."));
+    }
+    message.push_str(&format!("\nQuery ID is: {}", id));
+    message.push_str(&format!("\nCustomer ID: {}", customer_sub.to_string())); 
+    message.push_str("\rRequest method: ");
+    message.push_str(&req_method.as_str());
+    // TEST LOCATION
+    match get_user(&pool, customer_sub, &id).await {
+        Some(user) => message.push_str(&format!("\nfirst_name: {}\nlast_name: {}", user.first_name, user.last_name)),
+        None => message.push_str(&format!("\nNo matching user found"))
+    }
+
+    Ok(response(200, &message))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(service_fn(handler)).await
+}
---
+++ cognito_login.py
@@ -0,0 +1,116 @@
+import boto3
+import os
+import requests
+from dotenv import load_dotenv
+from botocore.exceptions import ClientError
+
+import json
+import hashlib
+import hmac
+import base64
+
+load_dotenv()
+USER_POOL_ID = os.getenv("USER_POOL_ID")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+APP_URL_BASE = os.getenv("APP_URL_BASE")
+
+
+def get_secret_hash():
+    # Calculate the secret hash using the client ID and secret
+    message = USERNAME + CLIENT_ID
+    digest = hmac.new(
+        key=CLIENT_SECRET.encode('utf-8'),
+        msg=message.encode('utf-8'),
+        digestmod=hashlib.sha256
+    ).digest()
+    return base64.b64encode(digest).decode()
+
+def sign_in(cognito_idp_client):
+    secret_hash = get_secret_hash()
+    client_id = CLIENT_ID
+    kwargs = {
+        "ClientId": client_id,
+        "AuthFlow": "USER_PASSWORD_AUTH",
+        "AuthParameters": {"USERNAME": USERNAME, "PASSWORD": PASSWORD, 'SECRET_HASH': secret_hash },
+    }
+    response = cognito_idp_client.initiate_auth(**kwargs)
+    
+    if response.get("ChallengeName") != None:
+        response = handle_new_password_challenge(cognito_idp_client, USERNAME, "P@55w0rd", response["Session"])
+
+    access_token = response['AuthenticationResult']['AccessToken']
+    #print(access_token)
+    return access_token
+
+def sign_out(cognito_idp_client, access_token):
+    cognito_idp_client.global_sign_out(
+        AccessToken=access_token
+    )
+    
+def handle_new_password_challenge(client, email, new_password, session):
+    try:
+        # Respond to the NEW_PASSWORD_REQUIRED challenge
+        response = client.respond_to_auth_challenge(
+            ClientId=CLIENT_ID,
+            ChallengeName='NEW_PASSWORD_REQUIRED',
+            ChallengeResponses={
+                'USERNAME': email,
+                'NEW_PASSWORD': new_password,
+                'SECRET_HASH': get_secret_hash(email)  # Include the secret hash again
+            },
+            Session=session  # Include the session from the previous response
+        )
+        
+        # Extract and return the new access token
+        return response
+
+    except ClientError as e:
+        print(f"An error occurred while responding to the challenge: {e.response['Error']['Message']}")
+        return None
+
+def refresh(client, refresh_token):
+    response = client.initiate_auth(
+            ClientId=CLIENT_ID,
+            AuthFlow='REFRESH_TOKEN_AUTH',
+            AuthParameters={
+                'REFRESH_TOKEN': refresh_token
+            }
+        )
+    return response
+
+def user_get(token):
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json"
+    }
+    params = {
+        "id": "ac6f7e1dafd35f549e916bc54fc88ee7894c6826c98f6a0a34fedee693493ce8"
+    }
+    response = requests.get(
+        APP_URL_BASE + "/users",
+        #APP_URL_BASE + "/test",
+        headers=headers,
+        params=params
+    )
+    if response.status_code == 200:
+        print("Response data:", response.json()['message'])  # or response.text for raw response
+    else:
+        print(f"Failed to make the request: {response.status_code} - {response.text}")
+
+def main(): 
+    cognito_idp_client = boto3.client('cognito-idp', region_name="us-west-1")
+    token = None
+    try:
+        token = sign_in(cognito_idp_client)
+        user_get(token)
+    finally:
+        if token:
+            sign_out(cognito_idp_client, token)
+            print("Signed out")
+
+
+if __name__=="__main__":
+    main()
